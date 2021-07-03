1be4785bbf3fe2b7a45091ed46a58638e69b1571
Kushaal Kumar Pothula <kushaalkumar.astronomer@gmail.com>
2021-07-03 13:20:50 +0530

    Added integrators: Euler-Cromer and Leapfrog

--- astrobody/solvers.pyx
+++ astrobody/solvers.pyx
@@ -43,8 +43,27 @@ def acceleration(np.ndarray[np.float64_t, ndim=2] pos, np.ndarray[np.float64_t, 
 
     return np.asarray(acc)
 
+def euler_cromer(np.ndarray[np.float64_t, ndim=2] pos, np.ndarray[np.float64_t, ndim=2] vel, np.ndarray[np.float64_t, ndim=2] acc):
+    cdef double dt = 1e-3 # Timestep
+    # The Euler-Cromer integration method
+    # v(t+1) = v(t) + a(t) x dt
+    # x(t+1) = x(t) + v(t+1) x dt
+    vel += acc * dt
+    pos += vel * dt
 
+    return pos
 
+def leapfrog(np.ndarray[np.float64_t, ndim=2] pos, np.ndarray[np.float64_t, ndim=2] vel, np.ndarray[np.float64_t, ndim=2] acc, np.ndarray[np.float64_t, ndim=1] mass):
+    cdef double dt = 1e-3 # Timestep
 
+    # The Leapfrog integration method
+    # v(t+1/2) = v(t) + a(t) x dt/2
+    # x(t+1) = x(t) + v(t+1/2) x dt
+    # a(t+1) = (G * m/((dx^2 + dy^2 + dz^2)^(3/2))) * dx * x
+    # v(t+1) = v(t+1/2) + a(t+1) x dt/2
+    vel += acc * dt/2
+    pos += vel * dt
+    acc = acceleration(pos, mass)
+    vel += acc * dt/2
 
-
+    return pos
