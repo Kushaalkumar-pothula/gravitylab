6e9229171ba0e883520b9b95781e608d7090c0c8
Kushaal Kumar Pothula <kushaalkumar.astronomer@gmail.com>
2021-07-08 00:36:24 +0530

    Updated to include package info

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from distutils.core import setup
+from sys import version
 from Cython.Build import cythonize
 import numpy
 
 setup(
+    name="GravityLab",
+    version="0.1",
+    packages=["gravitylab"],
+    author="Kushaal Kumar Pothula",
+    author_email="kushaalkumar.astronomer@gmail.com",
+    description="Astrophysical N-body simulator",
+    long_description=open("README.md").read(),
     ext_modules=cythonize("gravitylab/solvers.pyx"),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()],
+    install_requires = [
+        'numpy>=1.20.2',
+        'matplotlib>=3.3.4'
+    ]
 )
