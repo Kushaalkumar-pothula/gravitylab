4f85f6ee91ed8c6f4b61e9fd8eb9240fbf2aba2d
Kushaal Kumar Pothula <kushaalkumar.astronomer@gmail.com>
2021-07-12 20:24:37 +0530

    Added simulation module

--- gravitylab/simulate.py
+++ gravitylab/simulate.py
@@ -24,7 +24,7 @@ class simulation():
         self.vel = vel
         self.mass = mass
 
-    def run(self):
+    def run(self, verbose=False):
         """
         Run simulation
         """
@@ -36,24 +36,9 @@ class simulation():
         for _ in range(Nt):
             self.pos, acc = leapfrog(self.pos, self.vel, acc, self.mass, self.dt)
             pos_lst.append(self.pos.copy())
-
+        
         pos_arr = np.vstack(pos_lst)
+        if verbose==True:
+                print(pos_arr)
 
         return pos_arr
-
-    def plot(self, positions, three_dimensional=True, color = "blue", save=False):
-        """
-        Plot simulation output
-        """
-        fig = plt.figure()
-
-        if three_dimensional == True:
-            ax = plt.axes(projection='3d')
-            ax.scatter3D(positions[:,0], positions[:,1], positions[:,2], color = color)
-        else:
-            plt.scatter(positions[:,0], positions[:,1], positions[:,2], color = color)
-        
-        if save == True:
-            plt.savefig("gravitylab_output.png")
-        
-        plt.show()
