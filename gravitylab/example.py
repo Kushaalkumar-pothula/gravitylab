88f7926c45ef362ce187dd950595bf82ffe29dd2
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-07-12 20:36:40 +0530

    Changed simulation to Simulation

--- gravitylab/example.py
+++ gravitylab/example.py
@@ -1,4 +1,4 @@
-from simulate import simulation
+from simulate import Simulation
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,7 +13,7 @@ yi = pos[:,1]
 vel = np.random.uniform(size=(N,3))
 mass = np.ones(N)
 
-tenbody = simulation(N,dt,t,tEnd)
+tenbody = Simulation(N,dt,t,tEnd)
 tenbody.initial_conditions(pos,vel,mass)
 positions = tenbody.run()
 
