37fe63ce20af2dba2b02186a5493375aea542238
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-09-12 15:32:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ cmdclass = {"build_ext" : build_ext}
 
 setup(
     name="GravityLab",
-    version="9.1.1",
+    version="10.0.0",
     packages=["gravitylab"],
     license="MIT",
     author="Kushaal Kumar Pothula",
